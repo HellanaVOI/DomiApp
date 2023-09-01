dcb989e43b01f2a9b506ca83b89a3e2d9d68bbe1
Tony <izzi.tony@gmail.com>
2023-09-01 16:20:04 +0200

    Implementation d'un debut de Tab Bar

--- app/src/main/java/be/technifuture/domiapp/MainActivity.kt
+++ app/src/main/java/be/technifuture/domiapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package be.technifuture.domiapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.ContextMenu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import be.technifuture.domiapp.databinding.ActivityMainBinding
 
 
@@ -18,15 +21,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val toolbar = binding.toolBarCustom
-        binding.toolBarCustom.menuIcon.setOnClickListener {
-
-            val action = R.id.action_selectFragment_to_menuFragment
-            //val direction = SelectFragmentDirections.actionSelectFragmentToMenuFragment()
-            //findNavController().navigate(action)
-        }
-
-
         setSupportActionBar(toolbar.mainToolbar)
 
     }
+
+    fun showPopup(v: View) {
+        PopupMenu(this, v).apply {
+            // MainActivity implements OnMenuItemClickListener
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.language -> {
+                        Log.d("DEBUG", "1er Choix")
+                        true
+                    }
+                    R.id.filter -> {
+                        Log.d("DEBUG", "2e Choix")
+                        true
+                    }
+                    else -> false
+                }
+            }
+            inflate(R.menu.action)
+            show()
+        }
+    }
+
 }
